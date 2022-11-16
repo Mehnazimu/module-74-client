9c7b9c5cddf7712685acda118ce43d4c611d654a
Mehnazahmed <mehnazchowdhury613@gmail.com>
2022-11-16 16:46:47 +0600

    added

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main/Main";
+import Appointment from "../../Pages/Appointment/Appointment/Appointment";
+import DashBoard from "../../Pages/DashBoard/DashBoard";
+
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Signup from "../../Pages/Signup/Signup";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+            {
+                path: '/appointment',
+                element: <Appointment></Appointment>
+            }
+        ]
+    },
+    {
+        path: '/dashboard',
+        element: <PrivateRoute><DashBoard></DashBoard></PrivateRoute>
+    }
+])
+export default router;
