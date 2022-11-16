446564b18b7eaf027152b5ce2077eeffeb1c000e
Mehnazahmed <mehnazchowdhury613@gmail.com>
2022-11-16 17:18:59 +0600

    last

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -41,6 +41,9 @@ const BookingModal = ({ treatment, selectedDate, setTreatment, refetch }) => {
                     toast.success('Booking confirmed')
                     refetch();
                 }
+                else {
+                    toast.error(data.message)
+                }
 
             })
 
